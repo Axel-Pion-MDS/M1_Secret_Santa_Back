46946edc70faceff1c2cb4db904051087d470d94
Rémi Rubis <remi.rubis@yahoo.com>
2022-10-05 00:18:55 +0200

    feat(santa): Implementation of secret santa models

--- src/secretsanta/admin.py
+++ src/secretsanta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Santa, SantaMember
+
 # Register your models here.
+admin.site.register(Santa)
+admin.site.register(SantaMember)
--- src/secretsanta/models.py
+++ src/secretsanta/models.py
@@ -1,3 +1,33 @@
+from cProfile import label
 from django.db import models
 
-# Create your models here.
+from users.models import Member
+
+
+class Santa(models.Model):
+    label = models.CharField(max_length=255, unique=True)
+    description = models.TextField(max_length=500)
+    draw_date = models.DateTimeField()
+
+    def __str__(self):
+        return '{} - {} ({})'.format(self.pk, self.label, self.draw_date)
+
+
+class SantaMember(models.Model):
+    member = models.OneToOneField(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='member_id',
+    )
+    target = models.OneToOneField(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='target_id',
+    )
+    santa = models.ForeignKey(
+        Santa,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return '{} - {} {} > {} {}'.format(self.pk, self.member.firstname, self.member.lastname, self.target.firstname, self.member.lastname)
