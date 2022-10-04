1cf349767e9bb62affff8a225dbccc03608d35fb
Axel Pion <axel.pion@betc.com>
2022-10-04 15:32:49 +0200

    feat(): add crud for users and add models for promotions and roles

--- src/app/settings.py
+++ src/app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'promotions.apps.PromotionsConfig',
+    'roles.apps.RolesConfig',
+    'users.apps.UsersConfig',
+    'secretsanta.apps.SecretsantaConfig',
 ]
 
 MIDDLEWARE = [
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Member(models.Model):
+    email = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    promo = models.ForeignKey('promotions.promotion', on_delete=models.CASCADE, null=False)
+    role = models.ForeignKey('roles.role', on_delete=models.CASCADE, default=1, blank=True, null=True)
+
+    def __str__(self):
+        return '{} - {} {} {} {}'.format(self.pk, self.lastname, self.firstname, self.email, self.promo)
